5f26d59a2dd875a81ff93fbadf4607b07dbef19c
mmamalek <mmamalek@student.wethinkcode.co.za>
2019-08-22 07:11:19 +0200

    checker can now bw quit with CTRL+D

--- checker/checker.c
+++ checker/checker.c
@@ -3,6 +3,7 @@
 #include "../get_next_line/get_next_line.h"
 
 static void    perform_task(char *op, t_list **stack_a,t_list **stack_b);
+static void	run_last_test(t_list **a, t_list **b, char **arguments);
 
 int main(int ac, char **av)
 {
@@ -16,14 +17,15 @@ int main(int ac, char **av)
         read_list(++av, &stack_a);
         while (1)
         {
-            get_next_line(STDIN_FILENO, &input);
+            if (!get_next_line(STDIN_FILENO, &input))
+				break ;
             if (ft_strequ(input, "quit") || ft_strequ(input, "done"))
                 break;
             else
                 perform_task(input, &stack_a, &stack_b);
             input = NULL;
         }
-        //run_last_test(stack_a, stack_b, av);
+        run_last_test(&stack_a, &stack_b, av);
     }
     return (0);
 }
@@ -32,7 +34,7 @@ static void    perform_task(char *op, t_list **stack_a,t_list **stack_b)
 {
     if (ft_strequ(op, "sa"))
         sa(stack_a);
-    if (ft_strequ(op, "sb"))
+	else if (ft_strequ(op, "sb"))
         sb(stack_b);
     if (ft_strequ(op, "ss"))
         ss(stack_a, stack_b);
@@ -54,4 +56,24 @@ static void    perform_task(char *op, t_list **stack_a,t_list **stack_b)
        rrr(stack_a, stack_b);
     if (ft_strequ(op, "p") || ft_strequ(op, "print") || ft_strequ(op, "ls"))
         print_ab(*stack_a, *stack_b);
+}
+
+static void	run_last_test(t_list **a, t_list **b, char **arguments)
+{
+	int argc;
+
+	argc = 0;
+	if (!*b)
+	{
+		while (*arguments++)
+			argc++;
+		if (list_len(*a) == argc && list_is_ascending(*a))
+			ft_putendl("OK");
+		else
+			ft_putendl("KO");
+	}
+	else
+		ft_putendl("KO");
+	list_delete(a);
+	list_delete(b);
 }
