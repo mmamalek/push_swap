c7c7dd9a1d8eb4f6a5092cae69366e16998fcf9a
mmamalek <mmamalek@student.wethinkcode.co.za>
2019-08-25 16:52:42 +0200

    added thr r

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,17 +1,45 @@
 #include "push_swap.h"
 
+int		index_of(int num, t_list *list)
+{
+	int i;
+
+	if (list)
+	{
+		i = 0;
+		while (list && (list->data != num))
+		{
+			i++;
+			list = list->next;
+		}
+		if (list->data == num)
+			return (i);
+	}
+	return (-1);
+}
+
 void	bring_min_to_top(int min, int max, t_list **list)
 {
-	max = 0;
+	int min_position;
+
 	if (*list)
 	{
-		while ((*list)->data != min)
-		{
-			rotate_up(list);
-			ft_putendl("ra");
-		}
+		min_position = index_of(min, *list);
+		if ((min_position) <= (max - min + 1) / 2)
+			while ((*list)->data != min)
+			{
+				rotate_up(list);
+				ft_putendl("ra");
+			}
+		else
+			while ((*list)->data != min)
+			{
+				rotate_down(list);
+				ft_putendl("rra");
+			}
 	}
 }
+
 
 void do_push_b(t_list **a, t_list **b, int *min)
 {
@@ -60,7 +88,7 @@ int main(int ac, char **av)
 				do_push_b(&stack_a, &stack_b, &min_a);
 		}
 		push_all_to_a(&stack_a, &stack_b, &min_a);
-		print_ab(stack_a, stack_b);
+	//	print_ab(stack_a, stack_b);
 
 		
 	}
